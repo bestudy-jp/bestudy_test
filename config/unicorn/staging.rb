bbff31e1fde8784e1f8af06fc59f077976459b54
Makoto NAKAYA <nakaya@makoto-no-MacBook-Air.local>
2015-05-02 22:50:23 +0900

    ステージング環境用のデプロイ設定とUnicornの設定を追加しました。

---
+++ config/unicorn/staging.rb
@@ -0,0 +1,33 @@
+app_path = '/var/www/app/bestudy_test_staging'
+app_shared_path = "#{app_path}/shared"
+
+worker_processes 1
+
+working_directory "#{app_path}/current/"
+
+listen "#{app_shared_path}/tmp/sockets/unicorn.sock"
+listen 8080, tcp_nopush: true
+
+stdout_path "#{app_shared_path}/log/unicorn.stdout.log"
+stderr_path "#{app_shared_path}/log/unicorn.stderr.log"
+
+pid "#{app_shared_path}/tmp/pids/unicorn.pid"
+
+# ダウンタイムをなくす
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  unless old_pid == server.pid
+    begin
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
