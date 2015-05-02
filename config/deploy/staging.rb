3b64d784efe3a8ae17ea5244459912c3c3075744
Makoto NAKAYA <nakaya@makoto-no-MacBook-Air.local>
2015-05-02 23:56:59 +0900

    Productionのデータベース設定などを修正

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 set :branch, 'master'
-set :rails_env, 'production'
+set :rails_env, 'development'
 set :migration_role, 'db'
 
 set :deploy_to, '/var/www/app/bestudy_test_staging'
