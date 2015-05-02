d6bb08b47afe35debfc8724fd104b0f674a5cb35
sanryuu <cpu.cpu.cpu9@gmail.com>
2015-05-03 01:49:07 +0900

    開発用のアカウントの追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '820404027996736', '4d8d760f4c3ea0313afefe38794a25ec'
+
+  if Rails.env.production?
+    provider :facebook, '820404027996736', '4d8d760f4c3ea0313afefe38794a25ec'
+  else
+    provider :facebook, '835790176458121', 'cc6ddf8b380f611d16155fab0a5f190e'
+  end
 end
