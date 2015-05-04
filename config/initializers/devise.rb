92acb54ab1122308dd5842a513f9ae3349699232
Makoto NAKAYA <nakaya@makoto-no-MacBook-Air.local>
2015-05-05 02:50:13 +0900

    トップページ ログインのリンクを修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,5 @@
+require 'omniauth-facebook'
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -233,6 +235,11 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  if Rails.env.production?
+    config.omniauth(:facebook, '820404027996736', '4d8d760f4c3ea0313afefe38794a25ec', { scope: 'email' })
+  else
+    config.omniauth(:facebook, '835790176458121', 'cc6ddf8b380f611d16155fab0a5f190e', { scope: 'email' })
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-
   if Rails.env.production?
     provider :facebook, '820404027996736', '4d8d760f4c3ea0313afefe38794a25ec'
   else
