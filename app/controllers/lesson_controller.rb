8346d113823c9b84d7a79700225b79ff7c65cf09
Makoto NAKAYA <nakaya@makoto-no-MacBook-Air.local>
2015-05-05 06:57:21 +0900

    ログインしていない状態で購入を押すとエラーになる問題を修正しました。

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -7,10 +7,12 @@ class LessonController < ApplicationController
     @lesson = Lesson.find(params[:id]) if params[:id]
     redirect_to 'home' && return unless @lesson
 
-    UserPurchase.create(
-      user_id: current_user.id,
-      lesson_id: @lesson.id,
-      price: @lesson.price.to_i
-    )
+    if user_signed_in?
+      UserPurchase.create(
+        user_id: current_user.id,
+        lesson_id: @lesson.id,
+        price: @lesson.price.to_i
+      )
+    end
   end
 end
