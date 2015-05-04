2c132472856f793d98d6d0c363de0c8cee7c490a
Makoto NAKAYA <nakaya@makoto-no-MacBook-Air.local>
2015-05-05 07:03:12 +0900

    購入時のログイン後の遷移を修正

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -7,12 +7,15 @@ class LessonController < ApplicationController
     @lesson = Lesson.find(params[:id]) if params[:id]
     redirect_to 'home' && return unless @lesson
 
+    session[:user_return_to] = nil
     if user_signed_in?
       UserPurchase.create(
         user_id: current_user.id,
         lesson_id: @lesson.id,
         price: @lesson.price.to_i
       )
+    else
+      session[:user_return_to] = lesson_show(@lesson.id)
     end
   end
 end
